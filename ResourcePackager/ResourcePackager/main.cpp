3e045f5c1fc725abce4dc2f3d145838c4870e346
Yujie Liu <yujiel@fb.com>
2018-05-16 21:05:27 -0700

    Add gitignore

---
+++ ResourcePackager/ResourcePackager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ResourcePackager
+//
+//  Created by Yujie Liu on 5/16/18.
+//  Copyright © 2018 Yujie Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
