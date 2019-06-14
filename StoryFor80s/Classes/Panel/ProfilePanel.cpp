6962db4955c523dc689f8a93a88fc463c587c6f5
Yujie Liu <yujiel@fb.com>
2019-06-13 23:10:10 -0700

    2019年06月13日 星期四
    1. 补充个人资料页面，添加属性，技能熟练度，道具切页
    2. 添加自律相关规则，主角会因为压力太高偷懒

---
+++ StoryFor80s/Classes/Panel/ProfilePanel.cpp
@@ -0,0 +1,8 @@
+//
+//  ProfilePanel.cpp
+//  StoryFor80s
+//
+//  Created by Yujie Liu on 6/13/19.
+//
+
+#include "ProfilePanel.hpp"
---
+++ StoryFor80s/Classes/Panel/ProfilePanel.hpp
@@ -0,0 +1,13 @@
+//
+//  ProfilePanel.hpp
+//  StoryFor80s
+//
+//  Created by Yujie Liu on 6/13/19.
+//
+
+#ifndef ProfilePanel_hpp
+#define ProfilePanel_hpp
+
+#include <stdio.h>
+
+#endif /* ProfilePanel_hpp */
